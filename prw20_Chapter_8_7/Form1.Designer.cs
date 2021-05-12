65e90b31257e07ee59de10afbd61f5e0cc64b693
Preston Watson <prw20@txstate.edu>
2021-05-12 16:39:42 -0500

    Add project files.

---
+++ prw20_Chapter_8_7/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace prw20_Chapter_8_7
+{
+	partial class PigLatin
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnPig = new System.Windows.Forms.Button();
+			this.txtEnglish = new System.Windows.Forms.TextBox();
+			this.lblPigLatin = new System.Windows.Forms.Label();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.btnClear = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnPig
+			// 
+			this.btnPig.Location = new System.Drawing.Point(73, 195);
+			this.btnPig.Name = "btnPig";
+			this.btnPig.Size = new System.Drawing.Size(75, 23);
+			this.btnPig.TabIndex = 0;
+			this.btnPig.Text = "Translate";
+			this.btnPig.UseVisualStyleBackColor = true;
+			this.btnPig.Click += new System.EventHandler(this.btnPig_Click);
+			// 
+			// txtEnglish
+			// 
+			this.txtEnglish.Location = new System.Drawing.Point(137, 47);
+			this.txtEnglish.Name = "txtEnglish";
+			this.txtEnglish.Size = new System.Drawing.Size(100, 20);
+			this.txtEnglish.TabIndex = 1;
+			// 
+			// lblPigLatin
+			// 
+			this.lblPigLatin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblPigLatin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPigLatin.Location = new System.Drawing.Point(12, 70);
+			this.lblPigLatin.Name = "lblPigLatin";
+			this.lblPigLatin.Size = new System.Drawing.Size(372, 122);
+			this.lblPigLatin.TabIndex = 2;
+			this.lblPigLatin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblPigLatin.Visible = false;
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(235, 195);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(75, 23);
+			this.btnExit.TabIndex = 3;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// btnClear
+			// 
+			this.btnClear.Location = new System.Drawing.Point(154, 195);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(75, 23);
+			this.btnClear.TabIndex = 4;
+			this.btnClear.Text = "Clear";
+			this.btnClear.UseVisualStyleBackColor = true;
+			this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(93, 19);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(183, 25);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Let\'s try Pig Latin!";
+			// 
+			// PigLatin
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.ClientSize = new System.Drawing.Size(396, 230);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnClear);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.lblPigLatin);
+			this.Controls.Add(this.txtEnglish);
+			this.Controls.Add(this.btnPig);
+			this.Name = "PigLatin";
+			this.Text = "Pig Latin";
+			this.Load += new System.EventHandler(this.PigLatin_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnPig;
+		private System.Windows.Forms.TextBox txtEnglish;
+		private System.Windows.Forms.Label lblPigLatin;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.Button btnClear;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ prw20_Chapter_8_7/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prw20_Chapter_8_7
+{
+	public partial class PigLatin : Form
+	{
+		string firstLetter, restOfWord;
+		string pigWords = "";
+		string returnSentence = "";
+
+		public PigLatin()
+		{
+			InitializeComponent();
+		}
+
+
+		private void btnPig_Click(object sender, EventArgs e)
+		{
+			lblPigLatin.Text = "";
+			pigLatin();
+		}
+		private void btnClear_Click(object sender, EventArgs e)
+		{
+			Clear();
+		}
+
+		private void Clear()
+		{
+			lblPigLatin.Text = "";
+			txtEnglish.Text = "";
+			txtEnglish.Focus();
+
+			pigWords = "";
+			returnSentence = "";
+			firstLetter = "";
+			restOfWord = "";
+		}
+
+		private void pigLatin()
+		{
+
+			//Use a string array and the split function to get each word
+			//Get the first letter to move to the end 
+			//At the end of each entry into the array append the "ay" to the words
+			//Return the full sentence and insert it into the label.
+
+			string str = txtEnglish.Text.Trim();
+			string[] tokens = str.Split(null);
+			if (str != "")
+			{
+				foreach (string word in tokens)
+				{
+					firstLetter = word.Substring(0, 1);
+					restOfWord = word.Substring(1, word.Length - 1);
+					pigWords = restOfWord + firstLetter + "ay";
+					returnSentence += pigWords.ToUpper() + " ";
+				
+					//MessageBox.Show(pigWords.ToString());
+				}
+				lblPigLatin.Visible = true;
+				lblPigLatin.Text = returnSentence;
+			}
+			else
+			{
+				Clear();
+				MessageBox.Show("Please enter text");
+			}
+
+			/*for (int index = 0; index < tokens.Length; index++)
+			{
+				string tokens1 = tokens[index].Insert(0, tokens[index]);
+				StringBuilder s = new StringBuilder(tokens1 + "ay");
+				MessageBox.Show(s.ToString());
+			}*/
+		}
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void PigLatin_Load(object sender, EventArgs e)
+		{
+			//Whoops
+		}
+
+		
+	}
+}
